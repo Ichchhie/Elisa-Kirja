5314733897883821cb129cf80d39e1ad7784e16e
taranjotkaur28 <83290488+taranjotkaur28@users.noreply.github.com>
2024-03-12 19:05:42 +0200

    adding to repo

---
+++ WasmDemo-master/WasmDemo/composeApp/src/commonMain/kotlin/Greeting.kt
@@ -0,0 +1,8 @@
+class Greeting {
+//    private val platform = getPlatform()
+
+    fun greet(): String {
+        return "WELCOME TO THE ELISA BOOK\n" +
+                "The most popular Finnish audio and e-book service!"
+    }
+}
