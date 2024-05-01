2913ebd01afcdb2571707c182c3b7f077904ffb1
Ichchha Moktan <ichchhamoktan07@gmail.com>
2024-05-01 17:34:55 +0300

    show all books in a new page

--- composeApp/src/commonMain/kotlin/ui/BookUI.kt
+++ composeApp/src/commonMain/kotlin/ui/BookUI.kt
@@ -2,23 +2,43 @@ package ui
 
 import API.GetBooks
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material.Card
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material.icons.outlined.Star
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import cafe.adriel.voyager.navigator.LocalNavigator
+import cafe.adriel.voyager.navigator.currentOrThrow
 import coil3.compose.AsyncImage
-import kotlinx.coroutines.*
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
+import models.AllBooksContainer
 import models.BookCategory
 import models.BookContainer
 import models.Books
@@ -30,7 +50,6 @@ import wasmdemo.composeapp.generated.resources.headphones
 import wasmdemo.composeapp.generated.resources.menu_book
 
 class BookUI {
-
     // UI for a single book item
     @OptIn(ExperimentalResourceApi::class)
     @Composable
@@ -104,6 +123,7 @@ class BookUI {
     // UI for a single book category item
     @Composable
     fun categoryItem(category: BookCategory) {
+        val navigator = LocalNavigator.currentOrThrow
         val bookContainers = remember { mutableStateOf<List<BookContainer?>>(emptyList()) }
 
         // Fetch book data with LaunchedEffect
@@ -143,11 +163,48 @@ class BookUI {
             Modifier.fillMaxWidth().padding(top = 4.dp, end = 16.dp),
             horizontalAlignment = Alignment.End
         ) {
-            Text(
-                category.viewAllText,
-                color = AppStyles.primaryButtonColor,
-                textAlign = TextAlign.End
+            ClickableText(
+                text = AnnotatedString(category.viewAllText),
+                onClick = {
+                    navigator.push(AllBooksScreen())
+                },
+                style = TextStyle(
+                    color = AppStyles.primaryButtonColor,
+                )
             )
         }
     }
+
+    @Composable
+    fun allBooksItem() {
+        val bookContainers = remember { mutableStateOf<AllBooksContainer?>(null) }
+        // Fetch book data with LaunchedEffect
+        LaunchedEffect("all_books") {
+            val books = coroutineScope {
+                async {
+                    GetBooks().retrieveAllBooksOfCategory("483")
+                }
+            }
+            bookContainers.value = books.await()
+        }
+        if (bookContainers != null) {
+            val list = bookContainers.value?.record?.books
+            list?.let { books ->
+                LazyVerticalGrid(
+                    columns = GridCells.Adaptive(minSize = 250.dp),
+                    content = {
+                        items(books.size) { item ->
+                            bookItem(books[item], onItemClick = {
+                                // handle item click
+                            })
+                        }
+                    }
+                )
+            }
+        } else {
+            Text("data fetching")
+            // Show a loading indicator or an error message
+        }
+    }
 }
+
