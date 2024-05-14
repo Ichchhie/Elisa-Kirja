935e342a89fdfc3b2628db8423e3ef29179249b1
wisdom.kalu <wisdom.kalu@ashesi.edu.gh>
2024-05-15 00:35:04 +0300

    modified navbar skeleton

--- composeApp/src/commonMain/kotlin/ui/BottomNavItem.kt
+++ composeApp/src/commonMain/kotlin/ui/BottomNavItem.kt
@@ -9,14 +9,10 @@ import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MediumTopAppBar
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 
@@ -53,45 +49,5 @@ class BottomNavItem {
             }
         }
     }
-
-
-    @Composable
-    @OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterial3Api::class)
-    fun show(){
-        val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
-        Scaffold(
-            modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
-            topBar = {
-                MediumTopAppBar(
-                    title = {
-                        Text(
-                            "Medium TopAppBar",
-                            maxLines = 1,
-                            overflow = TextOverflow.Ellipsis
-                        )
-                    },
-                    navigationIcon = {
-                        IconButton(onClick = { /* doSomething() */ }) {
-                            Icon(
-                                imageVector = Icons.Filled.Menu,
-                                contentDescription = "Localized description"
-                            )
-                        }
-                    },
-                    actions = {
-                        IconButton(onClick = { /* doSomething() */ }) {
-                            Icon(
-                                imageVector = Icons.Filled.Favorite,
-                                contentDescription = "Localized description"
-                            )
-                        }
-                    },
-                    scrollBehavior = scrollBehavior
-                )
-            },
-            content = { }
-        )
-    }
-
 
 }
