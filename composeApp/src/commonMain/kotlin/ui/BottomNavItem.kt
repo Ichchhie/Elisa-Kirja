03a425d98fea6a54b73020916d25f6e121baab36
wisdom.kalu <wisdom.kalu@ashesi.edu.gh>
2024-05-15 02:25:45 +0300

    added a background color

--- composeApp/src/commonMain/kotlin/ui/BottomNavItem.kt
+++ composeApp/src/commonMain/kotlin/ui/BottomNavItem.kt
@@ -1,4 +1,5 @@
 package ui
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -15,6 +16,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 
@@ -25,6 +27,7 @@ class BottomNavItem {
     fun showing() {
         Row(
             modifier = Modifier
+                .background(color = Color.Blue)
                 .fillMaxWidth()
                 .padding(
                     start = 16.dp,
